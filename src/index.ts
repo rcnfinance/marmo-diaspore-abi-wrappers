3e4c74d7661669fe94e632f5bebbc06cfcd42c24
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-06-04 13:05:41 -0300

    format code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { Response } from './response';
 
-export * from './wrappers/debt_engine';
-export * from './wrappers/installments_model';
-export * from './wrappers/loan_manager';
+export * from './wrappers/debt_engine_wrapper';
+export * from './wrappers/installments_model_wrapper';
+export * from './wrappers/loan_manager_wrapper';
 
 
--- src/response.ts
+++ src/response.ts
@@ -1,10 +1,10 @@
 import { TransactionReceiptWithDecodedLogs } from 'ethereum-types';
 
 export class Response {
-    public txHash : string;
-    public receiptAsync : Promise<TransactionReceiptWithDecodedLogs>;
+    public txHash: string;
+    public receiptAsync?: Promise<TransactionReceiptWithDecodedLogs>;
 
-    constructor (txHash: string, receiptAsync: Promise<TransactionReceiptWithDecodedLogs>) {
+    constructor (txHash: string, receiptAsync?: Promise<TransactionReceiptWithDecodedLogs>) {
         this.txHash = txHash;
         this.receiptAsync = receiptAsync;
     }
