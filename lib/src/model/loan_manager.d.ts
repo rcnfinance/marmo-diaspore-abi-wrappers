21ca37f3bc224d927cc4342d97f0b86ba18f40e3
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-06-05 10:05:38 -0300

    update lib folder

--- lib/src/model/loan_manager.d.ts
+++ lib/src/model/loan_manager.d.ts
@@ -3,7 +3,16 @@ import { Contract, Provider, Wallet } from 'marmojs';
 export declare class LoanManager extends Contract {
     private _wallet;
     private _provider;
-    requestLoan(_amount: BigNumber, _model: string, _oracle: string, _borrower: string, _salt: BigNumber, _expiration: BigNumber, _loanData: string): Promise<void>;
-    constructor(contractAddress: string, wallet: Wallet, provider: Provider);
+    private execute;
+    requestLoan(_amount: BigNumber, _model: string, _oracle: string, _borrower: string, _salt: BigNumber, _expiration: BigNumber, _loanData: string): Promise<string>;
+    approveRequest(_id: string): Promise<string>;
+    registerApproveRequest(_id: string, _signature: string): Promise<string>;
+    lend(_id: string, _oracleData: string, _cosigner: string, _cosignerLimit: BigNumber, _cosignerData: string): Promise<string>;
+    cancel(_id: string): Promise<string>;
+    cosign(_id: string, _cost: BigNumber): Promise<string>;
+    settleLend(_requestData: string, _loanData: string, _cosigner: string, _maxCosignerCost: BigNumber, _cosignerData: string, _oracleData: string, _creatorSig: string, _borrowerSig: string): Promise<string>;
+    settleCancel(_requestData: string, _loanData: string): Promise<string>;
+    constructor(wallet: Wallet, provider: Provider);
+    init(contractAddress: Promise<string>): Promise<void>;
 }
 //# sourceMappingURL=loan_manager.d.ts.map
--- lib/src/wrappers/loan_manager_wrapper.d.ts
+++ lib/src/wrappers/loan_manager_wrapper.d.ts
@@ -7,6 +7,27 @@ export declare class LoanManagerMarmoContract {
     requestLoan: {
         sendTransactionAsync(_amount: BigNumber, _model: string, _oracle: string, _borrower: string, _salt: BigNumber, _expiration: BigNumber, _loanData: string, txData?: Partial<TxData>, estimateGasFactor?: number | undefined): Promise<Response>;
     };
-    constructor(contractAddress: string, wallet: Wallet, provider: Provider);
+    approveRequest: {
+        sendTransactionAsync(_id: string, txData?: Partial<TxData>, estimateGasFactor?: number | undefined): Promise<Response>;
+    };
+    registerApproveRequest: {
+        sendTransactionAsync(_id: string, _signature: string, txData?: Partial<TxData>, estimateGasFactor?: number | undefined): Promise<Response>;
+    };
+    lend: {
+        sendTransactionAsync(_id: string, _oracleData: string, _cosigner: string, _cosignerLimit: BigNumber, _cosignerData: string, txData?: Partial<TxData>, estimateGasFactor?: number | undefined): Promise<Response>;
+    };
+    cancel: {
+        sendTransactionAsync(_id: string, txData?: Partial<TxData>, estimateGasFactor?: number | undefined): Promise<Response>;
+    };
+    cosign: {
+        sendTransactionAsync(_id: string, _cost: BigNumber, txData?: Partial<TxData>, estimateGasFactor?: number | undefined): Promise<Response>;
+    };
+    settleLend: {
+        sendTransactionAsync(_requestData: string, _loanData: string, _cosigner: string, _maxCosignerCost: BigNumber, _cosignerData: string, _oracleData: string, _creatorSig: string, _borrowerSig: string, txData?: Partial<TxData>, estimateGasFactor?: number | undefined): Promise<Response>;
+    };
+    settleCancel: {
+        sendTransactionAsync(_requestData: string, _loanData: string, txData?: Partial<TxData>, estimateGasFactor?: number | undefined): Promise<Response>;
+    };
+    constructor(contractAddress: Promise<string>, wallet: Wallet, provider: Provider);
 }
 //# sourceMappingURL=loan_manager_wrapper.d.ts.map
