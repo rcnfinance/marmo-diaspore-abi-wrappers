545973868eb77325e79a610b7ccface18cf89598
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-06-12 15:37:03 -0300

    "TODO:" (installment model) done

--- src/model/installments_modal.ts
+++ src/model/installments_modal.ts
@@ -1,6 +1,42 @@
 import BigNumber = require("bn.js");
 import { IntentAction, Contract } from 'marmojs';
+import { AbstractContract } from "./abstract_contract";
 
-export class InstallmentsModel extends Contract {
-    //TODO:
+export class InstallmentsModel extends AbstractContract {
+
+    public async addPaid(
+        id: string,
+        amount: BigNumber
+    ): Promise<string> {
+        const intentAction: IntentAction = this.functionEncoder("addPaid", ['bytes32', 'uint256'])
+            .encode([id, amount]);
+        return this.execute(intentAction);
+    }
+
+    public async addDebt(
+        id: string,
+        amount: BigNumber
+    ): Promise<string> {
+        const intentAction: IntentAction = this.functionEncoder("addDebt", ['bytes32', 'uint256'])
+            .encode([id, amount]);
+        return this.execute(intentAction);
+    }
+
+    public async fixClock(
+        id: string,
+        target: BigNumber
+    ): Promise<string> {
+        const intentAction: IntentAction = this.functionEncoder("fixClock", ['bytes32', 'uint256'])
+            .encode([id, target]);
+        return this.execute(intentAction);
+    }
+
+    public async run(
+        id: string
+    ): Promise<string> {
+        const intentAction: IntentAction = this.functionEncoder("run", ['bytes32'])
+            .encode([id]);
+        return this.execute(intentAction);
+    }
+
 }
