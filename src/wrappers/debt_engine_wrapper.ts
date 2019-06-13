49ef602550e8bbae19fcd8126096542590f114d2
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-06-13 12:41:07 -0300

    [ADD] debt engine wrapper

--- src/wrappers/debt_engine_wrapper.ts
+++ src/wrappers/debt_engine_wrapper.ts
@@ -1,193 +1,200 @@
-import { BlockParam, BlockParamLiteral, CallData, ContractAbi, ContractArtifact, DecodedLogArgs, MethodAbi, TxData, TxDataPayable, SupportedProvider } from 'ethereum-types';
-import { BigNumber, classUtils, logUtils, providerUtils } from '@0x/utils';
+import { TxData } from 'ethereum-types';
+import { BigNumber } from '@0x/utils';
 import { Response } from '../Response';
-import * as ethers from 'ethers';
+import { DebtEngine } from '../model/debt_engine';
+import { Wallet, Provider } from 'marmojs';
 
 export class DebtEngineMarmoContract {
 
-    /*public approve = {
-        async sendTransactionAsync(
-            _operator: string,
-            _assetId: BigNumber,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        }
-    };
+    protected _debtEngine: DebtEngine;
 
-    public transferFrom = {
-        async sendTransactionAsync(
-            _from: string,
-            _to: string,
-            _assetId: BigNumber,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        }
-    };
-
-    public safeTransferFrom1 = {
-        async sendTransactionAsync(
-            _from: string,
-            _to: string,
-            _assetId: BigNumber,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        },
-
-    };
-    public setApprovalForAll = {
-        async sendTransactionAsync(
-            _operator: string,
-            _authorized: boolean,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        }
-    };
-    public safeTransferFrom2 = {
-        async sendTransactionAsync(
-            _from: string,
-            _to: string,
-            _assetId: BigNumber,
-            _userData: string,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        },
-    };
-    public create = {
-        async sendTransactionAsync(
-            _model: string,
-            _owner: string,
-            _oracle: string,
-            _data: string,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        }
-    };
-    public create2 = {
-        async sendTransactionAsync(
-            _model: string,
-            _owner: string,
-            _oracle: string,
-            _salt: BigNumber,
-            _data: string,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        }
-    };
-    public create3 = {
-        async sendTransactionAsync(
-            _model: string,
-            _owner: string,
-            _oracle: string,
-            _salt: BigNumber,
-            _data: string,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        }
-    };
-    public pay = {
-        async sendTransactionAsync(
-            _id: string,
-            _amount: BigNumber,
-            _origin: string,
-            _oracleData: string,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        }
-    };
-    public payToken = {
-        async sendTransactionAsync(
-            id: string,
-            amount: BigNumber,
-            origin: string,
-            oracleData: string,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        }
-    };
-    public payBatch = {
-        async sendTransactionAsync(
-            _ids: string[],
-            _amounts: BigNumber[],
-            _origin: string,
-            _oracle: string,
-            _oracleData: string,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        }
-    };
-    public payTokenBatch = {
-        async sendTransactionAsync(
-            _ids: string[],
-            _tokenAmounts: BigNumber[],
-            _origin: string,
-            _oracle: string,
-            _oracleData: string,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        }
-    };
-    public run = {
-        async sendTransactionAsync(
-            _id: string,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO:
-        }
-    };
-    public withdraw = {
-        async sendTransactionAsync(
-            _id: string,
-            _to: string,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO: 
-        }
-    };
-    public withdrawPartial = {
-        async sendTransactionAsync(
-            _id: string,
-            _to: string,
-            _amount: BigNumber,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO: 
-        }
+    public async approve(
+        _operator: string,
+        _assetId: BigNumber,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.approve(_operator, _assetId);
+        return new Response(intentId, undefined);
     }
-    public withdrawBatch = {
-        async sendTransactionAsync(
-            _ids: string[],
-            _to: string,
-            txData: Partial<TxData> = {},
-            estimateGasFactor?: number,
-        ): Promise<Response> {
-            //TODO: 
-        }
-    };*/
+
+    public async transferFrom(
+        _from: string,
+        _to: string,
+        _assetId: BigNumber,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.transferFrom(_from, _to, _assetId);
+        return new Response(intentId, undefined);
+    }
+
+
+    public async safeTransferFrom1(
+        _from: string,
+        _to: string,
+        _assetId: BigNumber,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.safeTransferFrom1(_from, _to, _assetId);
+        return new Response(intentId, undefined);
+    }
+
+    public async safeTransferFrom2(
+        _from: string,
+        _to: string,
+        _assetId: BigNumber,
+        _userData: string,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.safeTransferFrom2(_from, _to, _assetId, _userData);
+        return new Response(intentId, undefined);
+    }
+
+    public async setApprovalForAll(
+        _operator: string,
+        _authorized: boolean,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.setApprovalForAll(_operator, _authorized);
+        return new Response(intentId, undefined);
+    }
+
+
+    public async create(
+        _model: string,
+        _owner: string,
+        _oracle: string,
+        _data: string,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.create(_model, _owner, _oracle, _data);
+        return new Response(intentId, undefined);
+    }
+
+    public async create2(
+        _model: string,
+        _owner: string,
+        _oracle: string,
+        _salt: BigNumber,
+        _data: string,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.create2(_model, _owner, _oracle, _salt, _data);
+        return new Response(intentId, undefined);
+    }
+
+    public async create3(
+        _model: string,
+        _owner: string,
+        _oracle: string,
+        _salt: BigNumber,
+        _data: string,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.create3(_model, _owner, _oracle, _salt, _data);
+        return new Response(intentId, undefined);
+    }
+
+    public async pay(
+        _id: string,
+        _amount: BigNumber,
+        _origin: string,
+        _oracleData: string,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.pay(_id, _amount, _origin, _oracleData);
+        return new Response(intentId, undefined);
+    }
+
+    public async payToken(
+        _id: string,
+        _amount: BigNumber,
+        _origin: string,
+        _oracleData: string,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.payToken(_id, _amount, _origin, _oracleData);
+        return new Response(intentId, undefined);
+    }
+
+    public async payBatch(
+        _ids: string[],
+        _amounts: BigNumber[],
+        _origin: string,
+        _oracle: string,
+        _oracleData: string,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.payBatch(_ids, _amounts, _origin, _oracle, _oracle);
+        return new Response(intentId, undefined);
+    }
+
+    public async payTokenBatch(
+        _ids: string[],
+        _tokenAmounts: BigNumber[],
+        _origin: string,
+        _oracle: string,
+        _oracleData: string,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.payTokenBatch(_ids, _tokenAmounts, _origin, _oracle, _oracleData);
+        return new Response(intentId, undefined);
+    }
+
+    public async run(
+        _id: string,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.run(_id);
+        return new Response(intentId, undefined);
+    }
+
+    public async withdraw(
+        _id: string,
+        _to: string,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.withdraw(_id, _to);
+        return new Response(intentId, undefined); 
+    }
+
+    public async withdrawPartial(
+        _id: string,
+        _to: string,
+        _amount: BigNumber,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.withdrawPartial(_id, _to, _amount);
+        return new Response(intentId, undefined); 
+    }
+
+    public async withdrawBatch(
+        _ids: string[],
+        _to: string,
+        txData: Partial<TxData> = {},
+        estimateGasFactor?: number,
+    ): Promise<Response> {
+        const intentId = await this._debtEngine.withdrawBatch(_ids, _to);
+        return new Response(intentId, undefined); 
+    }
+
+    constructor(contractAddress: Promise<string>, wallet: Wallet, provider: Provider) {
+        this._debtEngine = new DebtEngine();
+        this._debtEngine.init(contractAddress, wallet, provider);
+    }
+
 }
